2913086beff4afd0b6b8c99e5d8ef90ee776a225
mohammed zguaoua <mohammedzgaoua2000@gmail.com>
2025-12-09 23:46:08 +0100

    Update settings.py

--- lessons_site/settings.py
+++ lessons_site/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jlf_g99q5ysd$rgpe7%!r@#vc0k1fa_zju&av%b8mc^swltf0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["smartlearnen.pythonanywhere.com"]
+ALLOWED_HOSTS = ['smartlearnen.pythonanywhere.com',
+                'www.smartlearnen.pythonanywhere.com']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
