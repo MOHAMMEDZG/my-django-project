afa42b39aa62a4eff41525504e8354ecbee1c741
mohammed zguaoua <mohammedzgaoua2000@gmail.com>
2025-12-09 17:55:52 +0100

    Update settings.py

--- lessons_site/settings.py
+++ lessons_site/settings.py
@@ -124,7 +124,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
