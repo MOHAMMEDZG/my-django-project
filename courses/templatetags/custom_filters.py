222ee20b157418e1fc1d5e6041685e3d19cc270c
mohammed zguaoua <mohammedzgaoua2000@gmail.com>
2025-12-12 14:52:31 +0100

    Delete courses/templatetags/custom_filters.py

--- courses/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def percentage(part, total):
-    try:
-        return (part / total) * 100
-    except:
-        return 0
