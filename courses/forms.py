5cdb7c20e6021d464cc156d2145936ca8a0b7f63
mohammed zguaoua <mohammedzgaoua2000@gmail.com>
2025-12-17 12:59:49 +0100

    Update forms.py

--- courses/forms.py
+++ courses/forms.py
@@ -21,26 +21,49 @@ class CommentForm(forms.ModelForm):
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ['title', 'description', 'category', 'image', 'video','level']
+        fields = [
+            'title',
+            'description',
+            'category',
+            'image',
+            'pdf_file',
+            'video_url',
+            'level',
+        ]
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-select'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-            'video': forms.ClearableFileInput(attrs={'class': 'form-control'}),
             'pdf_file': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'video_url': forms.URLInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'https://www.youtube.com/watch?v=xxxx'
+            }),
         }
 
 class InstructorCourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ['title', 'description', 'content', 'category', 'image', 'video','level']
+        fields = [
+            'title',
+            'description',
+            'content',
+            'category',
+            'image',
+            'pdf_file',
+            'video_url',
+            'level',
+        ]
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control'}),
             'image': forms.FileInput(attrs={'class': 'form-control'}),
-            'video': forms.FileInput(attrs={'class': 'form-control'}),
             'pdf_file': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'video_url': forms.URLInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'https://youtu.be/xxxx'
+            }),
         }
